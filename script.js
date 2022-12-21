d8a5af3494c49ae87a55f0999ef68875502210b9
DeverConor <deverconor@gmail.com>
2022-12-21 02:49:06 +0000

    added styling and added avascript for countdown

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 
 
 let timeSecond = 360;
-const timeH = document.querySelector("h3");
-
+const timeH = document.querySelector("#countdown");
+addEventListener.call
+console.log(timeH)
 displayTime(timeSecond);
 
 const countDown = setInterval(() => {
@@ -23,62 +24,122 @@ function displayTime(second) {
 }
 
 function endCount() {
-  timeH.innerHTML = "Times up";
+  timeH.innerHTML = "Times up! No Turkey for you!";
 }
 
 let btn = document.getElementById("button")
-let output = document.getElementById("state-list")
+// let output = document.getElementById("state-list") 
 
-btn.addEventListener('click', function(){
+btn.addEventListener('#button', function(){
     let input = document.getElementById("answer-field").Value;
-    if (input == 
-        Alabama,
-        Alaska,
-        Arizona,
-        Arkansas,
-        California,
-        Colorado,
-        Connecticut,
-        Delaware,
-        Florida,
-        Georgia,
-        Hawaii,
-        Idaho,
-        Illinois,
-        Indiana,
-        Iowa,
-        Kansas,
-        Kentucky,
-        Louisiana,
-        Maine,
-        Maryland,
-        Massachusetts,
-        Michigan,
-        Minnesota,
-        Mississippi,
-        Missouri,
-        Montana, Nebraska
-        Nevada,
-        New Hampshire,
-        New Jersey,
-        New Mexico,
-        New York,
-        North Carolina,
-        North Dakota,
-        Ohio,
-        Oklahoma,
-        Oregon,
-        Pennsylvania, Rhode Island
-        South Carolina,
-        South Dakota,
-        Tennessee,
-        Texas,
-        Utah,
-        Vermont,
-        Virginia,
-        Washington,
-        West Virginia,
-        Wisconsin,
-        Wyoming) {output.innerHTML.getElementById("state-list") = "userInput"
-    output.getElementById("scorebox")=+1}
-}
+    let output = document.getElementById("#state-list")
+    if  (input == "alabama" ||
+        input == "alaska" ||
+        input == "arizona"|| 
+        input == "arkansas"|| 
+        input == "california"|| 
+        input == "colorado"|| 
+        input == "connecticut"|| 
+        input == "delaware"|| 
+        input == "florida"||
+        input == "georgia" ||
+        input == "hawaii" ||
+        input == "idaho"|| 
+        input == "illinois"|| 
+        input == "indiana"|| 
+        input == "iowa"|| 
+        input == "kansas"|| 
+        input == "kentucky"|| 
+        input == "louisiana"||
+        input == "maine" ||
+        input == "maryland" ||
+        input == "massachusetts"|| 
+        input == "michigan"|| 
+        input == "minnesota"|| 
+        input == "mississippi"|| 
+        input == "missouri"|| 
+        input == "montana"|| 
+        input == "nebraska"||
+        input == "nevada"||
+        input == "new hampshire"||
+        input == "new jersey"|| 
+        input == "new mexico"|| 
+        input == "new york"|| 
+        input == "north carolina"|| 
+        input == "north dakota"|| 
+        input == "north dakota"|| 
+        input == "oklahoma"||
+        input == "oregon" ||
+        input == "pennsylvania" ||
+        input == "rhode island"|| 
+        input == "south carolina"|| 
+        input == "south dakota"|| 
+        input == "tennessee"|| 
+        input == "texas"|| 
+        input == "utah"|| 
+        input == "vermont"||
+        input == "virginia" ||
+        input == "washington" ||
+        input == "west virginia"|| 
+        input == "wisconsin"|| 
+        input == "wyoming"||) {
+          output = document.getElementByID = ("#state-list")
+          console.log("<li>[input]</li>)
+        }
+          // output.innerHTML = "<li>" + input + "</li>"
+        // }
+      
+//       }
+//     output.getElementById("scorebox")=+1}
+//     else (input =!
+//       Alabama,
+//         Alaska,
+//         Arizona,
+//         Arkansas,
+//         California,
+//         Colorado,
+//         Connecticut,
+//         Delaware,
+//         Florida,
+//         Georgia,
+//         Hawaii,
+//         Idaho,
+//         Illinois,
+//         Indiana,
+//         Iowa,
+//         Kansas,
+//         Kentucky,
+//         Louisiana,
+//         Maine,
+//         Maryland,
+//         Massachusetts,
+//         Michigan,
+//         Minnesota,
+//         Mississippi,
+//         Missouri,
+//         Montana, 
+//         Nebraska,
+//         Nevada,
+//         New Hampshire,
+//         New Jersey,
+//         New Mexico,
+//         New York,
+//         North Carolina,
+//         North Dakota,
+//         Ohio,
+//         Oklahoma,
+//         Oregon,
+//         Pennsylvania, 
+//         Rhode Island,
+//         South Carolina,
+//         South Dakota,
+//         Tennessee,
+//         Texas,
+//         Utah,
+//         Vermont,
+//         Virginia,
+//         Washington,
+//         West Virginia,
+//         Wisconsin,
+//         Wyoming)
+//')'
